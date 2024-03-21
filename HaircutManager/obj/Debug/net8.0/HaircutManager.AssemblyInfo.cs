7c39540f1712108e1f740865655a7065d058a48f
Domati <mateuszdomagalski4@gmail.com>
2024-03-21 09:44:20 +0100

    Działające logowanie

--- HaircutManager/obj/Debug/net8.0/HaircutManager.AssemblyInfo.cs
+++ HaircutManager/obj/Debug/net8.0/HaircutManager.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HaircutManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8871fd5cb6ddf65b308ad789c606486135bdaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edf87076b20069a6a9a2ad7e10c903bac59e14bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HaircutManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HaircutManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
